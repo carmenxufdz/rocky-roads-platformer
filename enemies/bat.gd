6a2f733ad6ef274de4d641c8ba4becd73890e85e
Carmenchu <carmenxfdz@gmail.com>
2024-11-26 13:50:41 +0100

    follows you enemy

--- enemies/bat.gd
+++ enemies/bat.gd
@@ -9,11 +9,34 @@ class_name Bat
 @export var SPEED: float = 25
 @export var DETECTION_RADIUS: float = 100.0  # Distancia para empezar a perseguir
 
+var target: Node2D = null
+
+func set_target(new_target: Node2D) -> void:
+	target = new_target
+	navigation_agent_2d.target_position = target.global_position
+	print("Target asignado: ", target.name)
+
 
 func _ready() -> void:
 	add_to_group("killable_enemies")
+	add_to_group("navigation_enemies")
 
 func _physics_process(delta: float) -> void:
-	animatedSprite.play("sleep")
+	if not target:
+		animatedSprite.play("sleep")
+		return
 
+	navigation_agent_2d.target_position = target.global_position
+
+	#if navigation_agent_2d.is_navigation_finished():
+	#	velocity = Vector2.ZERO
+	#else:
+	var next_path_position = navigation_agent_2d.get_next_path_position()
+	var direction = (next_path_position - global_position).normalized()
+	velocity = direction * SPEED
+	if direction.x > 0:
+		animatedSprite.flip_h = false
+	else:
+		animatedSprite.flip_h = true
+	animatedSprite.play("flying")
 	move_and_slide()
--- world/level.gd
+++ world/level.gd
@@ -19,6 +19,8 @@ func _ready() -> void:
 	player.connect_camera(camera)
 	player_spawn_location = player.global_position
 	
+	assign_targets_to_enemies()
+	
 	Events.connect("coin_collected", Callable(self, "_coin_collected"), CONNECT_DEFERRED)
 	Events.connect("player_died",Callable(self, "_player_died"),CONNECT_DEFERRED)
 	Events.connect("hit_checkpoint",Callable(self, "_on_hit_checkpoint"),CONNECT_DEFERRED)
@@ -43,3 +45,13 @@ func _on_hit_checkpoint(checkpoint_position):
 func _coin_collected(value: int) -> void:
 	score += value
 	print("Moneda recogida. Puntos: ", score)
+
+func assign_targets_to_enemies():
+	var enemies = enemies_container.get_children()
+	for enemy in enemies:
+		if enemy.is_in_group("navigation_enemies"):
+			if enemy.has_method("set_target"):
+				enemy.set_target(player)
+				print("Asignado Player a ", enemy.name)
+			else:
+				print("El enemigo ", enemy.name, " no tiene el método 'set_target'.")
