7d3c047a10a65c1ab807adb28eacb91f5b27cb8e
Carmenchu <carmenxfdz@gmail.com>
2024-11-27 23:43:28 +0100

    Starsworks

--- UI/coin.gd
+++ UI/coin.gd
@@ -2,20 +2,27 @@ extends Area2D
 
 class_name Coin
 
+@onready var addAnimation := $AddAnimatedSprite
+@onready var animatedSprite := $AnimatedSprite2D
+
 @export var value: int = 1
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$AnimatedSprite2D.play("default")
+	animatedSprite.play("default")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
 		Events.emit_signal("coin_collected", value)
 		SoundPlayer.play_sound(SoundPlayer.COIN)
-		queue_free()
-	
+		animatedSprite.hide()
+		addAnimation.show()
+		addAnimation.play("default")
+		addAnimation.animation_finished.connect(_finished)
+		
+func _finished() -> void:
+	queue_free()
